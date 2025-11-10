570ae3800f9d44ca8ea3f3e45f55d920d306a2e0
marek.tremel <tremelmarek@gmail.com>
2025-11-10 21:16:30 +0100

    feat: add operation IDs to course list and create endpoints

--- api/app/src/courses/routers.py
+++ api/app/src/courses/routers.py
@@ -8,11 +8,11 @@ from app.database import SessionSqlSessionDependency
 router = APIRouter(prefix="/courses", tags=["Courses"])
 
 
-@router.get("")
+@router.get("", operation_id="list_courses")
 async def list_courses(db: SessionSqlSessionDependency):
     return get_courses(db)
 
 
-@router.post("")
+@router.post("", operation_id="create_course")
 async def endp_create_course(course: CourseCreate, db: SessionSqlSessionDependency):
     return create_course(db, course)
