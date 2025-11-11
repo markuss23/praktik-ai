1c42ce1e8ad6828d0b4fd3b2c74967531b7ee64e
marek.tremel <tremelmarek@gmail.com>
2025-11-11 20:14:01 +0100

    feat: add error handling for course retrieval and update operations

--- api/app/src/courses/controllers.py
+++ api/app/src/courses/controllers.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy import Select, Sequence, select
 from sqlalchemy.orm import Session
 
@@ -24,3 +25,35 @@ def create_course(db: Session, course_data: CourseCreate) -> Course:
     db.refresh(course)
 
     return course
+
+
+def get_course(db: Session, course_id: int) -> Course:
+    stm: Select[tuple[models.Course]] = select(models.Course).where(
+        models.Course.course_id == course_id
+    )
+
+    result: models.Course | None = db.execute(stm).scalars().first()
+
+    if result is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+
+    return Course.model_validate(result)
+
+
+def update_course(db: Session, course_id: int, course_data: CourseCreate) -> Course:
+    stm: Select[tuple[models.Course]] = select(models.Course).where(
+        models.Course.course_id == course_id
+    )
+
+    course: models.Course | None = db.execute(stm).scalars().first()
+
+    if course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+
+    for key, value in course_data.model_dump().items():
+        setattr(course, key, value)
+
+    db.commit()
+    db.refresh(course)
+
+    return Course.model_validate(course)
--- api/app/src/courses/routers.py
+++ api/app/src/courses/routers.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter
 
 from app.src.courses.schemas import CourseCreate, Course
-from app.src.courses.controllers import create_course, get_courses
+from app.src.courses.controllers import (
+    create_course,
+    get_courses,
+    get_course,
+    update_course,
+)
 from app.database import SessionSqlSessionDependency
 
 
@@ -14,5 +19,19 @@ async def list_courses(db: SessionSqlSessionDependency) -> list[Course]:
 
 
 @router.post("", operation_id="create_course")
-async def endp_create_course(course: CourseCreate, db: SessionSqlSessionDependency) -> Course:
+async def endp_create_course(
+    course: CourseCreate, db: SessionSqlSessionDependency
+) -> Course:
     return create_course(db, course)
+
+
+@router.get("/{course_id}", operation_id="get_course")
+async def endp_get_course(course_id: int, db: SessionSqlSessionDependency) -> Course:
+    return get_course(db, course_id)
+
+
+@router.put("/{course_id}", operation_id="update_course")
+async def endp_update_course(
+    course_id: int, course: CourseCreate, db: SessionSqlSessionDependency
+) -> Course:
+    return update_course(db, course_id, course)
